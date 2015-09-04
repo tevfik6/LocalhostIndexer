76edf8c8571c632e4ade8f8b1e9ab63f364ef6a3
Tevfik Tümer <tevfik6@gmail.com>
2015-09-04 11:39:31 +0200

    Fix: Level Up shows Sugar Icon if parent folder is a sugar instance

--- body.php
+++ body.php
@@ -372,8 +372,9 @@ require_once "breadcrumbs.php";
 
 			$sugar_version_edition = '<span class="sugar"><span class="version" data="'.$version_sort_value.'">'.$sugar['version'].'</span><span class="flavor">'.$sugar['flavor'].'</span><span class="build">'.$sugar['build'].'</span></span>';
 			$any_sugar_folder = true;
-
-			$icon = '<img class="sugar_logo" src="assets/img/sugar_logo.svg"> ';
+			
+			if($file_folder_name != "..")
+				$icon = '<img class="sugar_logo" src="assets/img/sugar_logo.svg"> ';
 		}
 
 		$file_folder_info .= '<span class="file_folder_size" data="'.$file_folder->getSize().'">'.$file_folder_size.'</span>';
